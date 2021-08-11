44d6821395c9308e1af15d59cb215d5b3f96cd3f
Jan De Wilde <jan.de.wilde87@gmail.com>
2021-08-11 23:30:09 +0200

    feat: add latitude and longitude to all remaining playgrounds

--- src/app/services/playgrounds.service.ts
+++ src/app/services/playgrounds.service.ts
@@ -57,7 +57,9 @@ const playgrounds: Array<Playground> = [
     municipality: 'Deinze',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['CLIMB', 'FOOTBALL', 'CABLEWAY', 'SLIDE', 'SPRING_RIDER', 'BENCH', 'SWING', 'WIRE_CIRCUS', 'SAND', 'HILL']
+    activities: ['CLIMB', 'FOOTBALL', 'CABLEWAY', 'SLIDE', 'SPRING_RIDER', 'BENCH', 'SWING', 'WIRE_CIRCUS', 'SAND', 'HILL'],
+    lat: 50.991947456939656,
+    lng: 3.5226550928358202
   },
   {
     id: 4,
@@ -75,7 +77,9 @@ const playgrounds: Array<Playground> = [
     municipality: 'Deinze',
     targetAgeFrom: 6,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'BALANCE', 'SLIDE', 'DIVING_RACK', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'BALANCE', 'SLIDE', 'DIVING_RACK', 'SWING', 'BENCH'],
+    lat: 50.985049531936475,
+    lng: 3.5366737026468744
   },
   {
     id: 6,
@@ -95,7 +99,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['SLIDE', 'BENCH', 'SWING', 'SAND', 'SPRING_RIDER', 'CLIMB']
+    activities: ['SLIDE', 'BENCH', 'SWING', 'SAND', 'SPRING_RIDER', 'CLIMB'],
+    lat: 50.9750120334014,
+    lng: 3.5431717154944793
   },
   {
     id: 8,
@@ -126,7 +132,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 3,
     targetAgeTo: 8,
-    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH'],
+    lat: 50.966950664314446,
+    lng: 3.5320755672939486
   },
   {
     id: 11,
@@ -135,7 +143,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH'],
+    lat: 50.96761957117237,
+    lng: 3.533867282886243
   },
   {
     id: 12,
@@ -155,7 +165,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 3,
     targetAgeTo: 14,
-    activities: ['CLIMB', 'BASKETBALL', 'PICNIC_BENCH', 'SLIDE', 'CHILL', 'SWING', 'SPRING_RIDER']
+    activities: ['CLIMB', 'BASKETBALL', 'PICNIC_BENCH', 'SLIDE', 'CHILL', 'SWING', 'SPRING_RIDER'],
+    lat: 50.96503177089397,
+    lng: 3.5246659843552273
   },
   {
     id: 14,
@@ -164,16 +176,20 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 6,
     targetAgeTo: 14,
-    activities: ['CLIMB', 'PICNIC_BENCH', 'BALANCE', 'CHILL']
+    activities: ['CLIMB', 'PICNIC_BENCH', 'BALANCE', 'CHILL'],
+    lat: 50.97016805398308,
+    lng: 3.521932983988534
   },
   {
     id: 15,
-    name: 'Kleine Oostkouterstraat',
+    name: 'Kleine Oostkouter',
     municipality: 'Deinze',
     submunicipality: 'Petegem',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['SLIDE', 'BENCH', 'SWING', 'SAND', 'SPRING_RIDER']
+    activities: ['SLIDE', 'BENCH', 'SWING', 'SAND', 'SPRING_RIDER'],
+    lat: 50.971706006761416,
+    lng: 3.5236205906770475
   },
   {
     id: 16,
@@ -182,7 +198,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'SPRING_RIDER', 'SLIDE', 'PICNIC_BENCH', 'BALANCE']
+    activities: ['CLIMB', 'SPRING_RIDER', 'SLIDE', 'PICNIC_BENCH', 'BALANCE'],
+    lat: 50.97391991511694,
+    lng: 3.524304808411402
   },
   {
     id: 17,
@@ -191,7 +209,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 6,
     targetAgeTo: 14,
-    activities: ['CLIMB', 'BENCH', 'SLIDE', 'BALANCE']
+    activities: ['CLIMB', 'BENCH', 'SLIDE', 'BALANCE'],
+    lat: 50.975047591988925,
+    lng: 3.519449309572729
   },
   {
     id: 18,
@@ -200,7 +220,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Petegem',
     targetAgeFrom: 2,
     targetAgeTo: 8,
-    activities: ['CLIMB', 'BENCH', 'SLIDE', 'SPRING_RIDER']
+    activities: ['CLIMB', 'BENCH', 'SLIDE', 'SPRING_RIDER'],
+    lat: 50.97511205259347,
+    lng: 3.5188760155317116
   },
   {
     id: 19,
@@ -209,7 +231,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Grammene',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'SPIN', 'DIVING_RACK', 'SLIDE', 'BALANCE', 'BENCH', 'SWING', 'SPRING_RIDER', 'SAND']
+    activities: ['CLIMB', 'SPIN', 'DIVING_RACK', 'SLIDE', 'BALANCE', 'BENCH', 'SWING', 'SPRING_RIDER', 'SAND'],
+    lat: 50.97786095967864,
+    lng: 3.469991652155758
   },
   {
     id: 20,
@@ -218,7 +242,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Gottem',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['CLIMB', 'CHILL', 'SAND', 'BALANCE', 'PICNIC_BENCH', 'FOOTBALL', 'BENCH']
+    activities: ['CLIMB', 'CHILL', 'SAND', 'BALANCE', 'PICNIC_BENCH', 'FOOTBALL', 'BENCH'],
+    lat: 50.96394462230283,
+    lng: 3.461851763052604
   },
   {
     id: 21,
@@ -227,7 +253,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Wontergem',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['CLIMB', 'FOOTBALL', 'SAND', 'SLIDE', 'BASKETBALL', 'BALANCE', 'BENCH']
+    activities: ['CLIMB', 'FOOTBALL', 'SAND', 'SLIDE', 'BASKETBALL', 'BALANCE', 'BENCH'],
+    lat: 50.9793388041597,
+    lng: 3.440202322867133
   },
   {
     id: 22,
@@ -236,7 +264,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Vinkt',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['CLIMB', 'FOOTBALL', 'SAND', 'SLIDE', 'WIRE_CIRCUS', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'FOOTBALL', 'SAND', 'SLIDE', 'WIRE_CIRCUS', 'SWING', 'BENCH'],
+    lat: 51.00696474859768,
+    lng: 3.475648116451678
   },
   {
     id: 23,
@@ -245,7 +275,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Zeveren',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'BALANCE', 'SLIDE', 'BENCH', 'SWING', 'SAND']
+    activities: ['CLIMB', 'BALANCE', 'SLIDE', 'BENCH', 'SWING', 'SAND'],
+    lat: 50.99642896531785,
+    lng: 3.4952641930229684
   },
   {
     id: 24,
@@ -254,7 +286,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Meigem',
     targetAgeFrom: 3,
     targetAgeTo: 8,
-    activities: ['CLIMB', 'BALANCE', 'BENCH', 'SLIDE', 'SPRING_RIDER', 'SAND', 'SWING', 'PICNIC_BENCH']
+    activities: ['CLIMB', 'BALANCE', 'BENCH', 'SLIDE', 'SPRING_RIDER', 'SAND', 'SWING', 'PICNIC_BENCH'],
+    lat: 51.015711269063885,
+    lng: 3.53988549483423
   },
   {
     id: 25,
@@ -263,7 +297,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Sint-Martens-Leerne',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'FOOTBALL', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'FOOTBALL', 'SWING', 'BENCH'],
+    lat: 51.017602738981005,
+    lng: 3.580715315983599
   },
   {
     id: 26,
@@ -272,7 +308,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Bachte-Maria-Leerne',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['CLIMB', 'FOOTBALL', 'BENCH', 'SLIDE', 'SPRING_RIDER', 'SAND', 'SWING', 'WIRE_CIRCUS']
+    activities: ['CLIMB', 'FOOTBALL', 'BENCH', 'SLIDE', 'SPRING_RIDER', 'SAND', 'SWING', 'WIRE_CIRCUS'],
+    lat: 51.00651175175804,
+    lng: 3.580539800093541
   },
   {
     id: 27,
@@ -281,7 +319,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Bachte-Maria-Leerne',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['CLIMB', 'PICNIC_BENCH', 'SLIDE', 'SWING']
+    activities: ['CLIMB', 'PICNIC_BENCH', 'SLIDE', 'SWING'],
+    lat: 51.00421258895883,
+    lng: 3.5701533827468643
   },
   {
     id: 28,
@@ -290,7 +330,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Astene',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'SWING', 'DIVING_RACK']
+    activities: ['CLIMB', 'SWING', 'DIVING_RACK'],
+    lat: 50.9898357861625,
+    lng: 3.5699327395879332
   },
   {
     id: 29,
@@ -299,7 +341,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Astene',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH', 'FOOTBALL']
+    activities: ['SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH', 'FOOTBALL'],
+    lat: 50.98998935149279,
+    lng: 3.572694795863018
   },
   {
     id: 30,
@@ -308,7 +352,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Astene',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['CLIMB', 'SPIN', 'PICNIC_BENCH', 'SLIDE', 'FOOTBALL', 'BENCH', 'SWING', 'SPRING_RIDER', 'SAND']
+    activities: ['CLIMB', 'SPIN', 'PICNIC_BENCH', 'SLIDE', 'FOOTBALL', 'BENCH', 'SWING', 'SPRING_RIDER', 'SAND'],
+    lat: 50.98093228237019,
+    lng: 3.5683992483695546
   },
   {
     id: 31,
@@ -317,7 +363,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Astene',
     targetAgeFrom: 3,
     targetAgeTo: 16,
-    activities: ['FOOTBALL']
+    activities: ['FOOTBALL'],
+    lat: 50.987076142057845,
+    lng: 3.562966384576815
   },
   {
     id: 32,
@@ -326,7 +374,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Astene',
     targetAgeFrom: 2,
     targetAgeTo: 14,
-    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'BALANCE', 'SAND', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH'],
+    lat: 50.98762506106999,
+    lng: 3.5645389205083897
   },
   {
     id: 33,
@@ -335,7 +385,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Nevele',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'SPIN', 'HILL', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'SPIN', 'HILL', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH'],
+    lat: 51.034617399767825,
+    lng: 3.543628435288175
   },
   {
     id: 34,
@@ -343,7 +395,9 @@ const playgrounds: Array<Playground> = [
     municipality: 'Deinze',
     submunicipality: 'Nevele',
     targetAgeFrom: 6,
-    activities: ['SKATE_BLADE']
+    activities: ['SKATE_BLADE'],
+    lat: 51.033636090093566,
+    lng: 3.554056074089979
   },
   {
     id: 35,
@@ -352,7 +406,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Vosselare',
     targetAgeFrom: 3,
     targetAgeTo: 14,
-    activities: ['CLIMB', 'BALANCE', 'SPRING_RIDER', 'SLIDE', 'FOOTBALL', 'BENCH', 'SWING', 'CHILL']
+    activities: ['CLIMB', 'BALANCE', 'SPRING_RIDER', 'SLIDE', 'FOOTBALL', 'BENCH', 'SWING', 'CHILL'],
+    lat: 51.02488131945519,
+    lng: 3.558657777228252
   },
   {
     id: 36,
@@ -361,7 +417,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Poesele',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'SPIN', 'SLIDE', 'BALANCE', 'SWING', 'SPRING_RIDER']
+    activities: ['CLIMB', 'SPIN', 'SLIDE', 'BALANCE', 'SWING', 'SPRING_RIDER'],
+    lat: 51.03455722748165,
+    lng: 3.518032072234641
   },
   {
     id: 37,
@@ -370,7 +428,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Hansbeke',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'SPRING_RIDER', 'SLIDE', 'SWING']
+    activities: ['CLIMB', 'SPRING_RIDER', 'SLIDE', 'SWING'],
+    lat: 51.07411326726052,
+    lng: 3.537044465417952
   },
   {
     id: 38,
@@ -379,16 +439,20 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Merendree',
     targetAgeFrom: 3,
     targetAgeTo: 14,
-    activities: ['CLIMB', 'BALANCE', 'PICNIC_BENCH', 'SLIDE', 'FOOTBALL', 'SWING', 'SPRING_RIDER']
+    activities: ['CLIMB', 'BALANCE', 'PICNIC_BENCH', 'SLIDE', 'FOOTBALL', 'SWING', 'SPRING_RIDER'],
+    lat: 51.08024660594705,
+    lng: 3.5792407171916856
   },
   {
     id: 39,
-    name: 'Ter Kale-Jan Burssenstraat',
+    name: 'Ter Kale - Jan Burssenstraat',
     municipality: 'Deinze',
     submunicipality: 'Merendree',
     targetAgeFrom: 3,
     targetAgeTo: 8,
-    activities: ['CLIMB', 'BALANCE', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH']
+    activities: ['CLIMB', 'BALANCE', 'SLIDE', 'SPRING_RIDER', 'SWING', 'BENCH'],
+    lat: 51.075153897138904,
+    lng: 3.575585745626276
   },
   {
     id: 40,
@@ -397,7 +461,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Landegem',
     targetAgeFrom: 3,
     targetAgeTo: 12,
-    activities: ['CLIMB', 'SPIN', 'HILL', 'SLIDE', 'BALANCE', 'SWING', 'SPRING_RIDER']
+    activities: ['CLIMB', 'SPIN', 'HILL', 'SLIDE', 'BALANCE', 'SWING', 'SPRING_RIDER'],
+    lat: 51.06189826213017,
+    lng: 3.5798533793276546
   },
   {
     id: 41,
@@ -406,7 +472,9 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Landegem',
     targetAgeFrom: 3,
     targetAgeTo: 8,
-    activities: ['CLIMB', 'BENCH', 'SLIDE', 'SPRING_RIDER']
+    activities: ['CLIMB', 'BENCH', 'SLIDE', 'SPRING_RIDER'],
+    lat: 51.055640512600625,
+    lng: 3.5836988896539665
   },
   {
     id: 42,
@@ -415,6 +483,8 @@ const playgrounds: Array<Playground> = [
     submunicipality: 'Landegem',
     targetAgeFrom: 3,
     targetAgeTo: 14,
-    activities: ['CLIMB', 'CHILL', 'BALANCE', 'SPRING_RIDER', 'FOOTBALL', 'SAND']
+    activities: ['CLIMB', 'CHILL', 'BALANCE', 'SPRING_RIDER', 'FOOTBALL', 'SAND'],
+    lat: 51.05428913363674,
+    lng: 3.5767548751622806
   },
 ];
